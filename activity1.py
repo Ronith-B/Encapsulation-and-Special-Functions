b9b9d35faa0cfa4530ece912ea84668af7a74015
Ronith-B <2011ronith@gmail.com>
2024-11-01 10:12:22 +0000

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,16 @@
+
+class myClass:
+
+    __privateVar = 19;
+
+
+    def __privMeth(self):
+        print("I'm inside class myClass")
+
+   
+    def hello(self):
+        print("Private Variable value: ", myClass.__privateVar)
+
+foo = myClass()
+foo.hello()
+foo.__privMeth
